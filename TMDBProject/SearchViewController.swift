43cfbd970abdcb9d2aa03e9567c080987d6bfe49
idhun90 <idhun90@naver.com>
2022-08-03 09:26:58 +0900

    Initial Commit

---
+++ TMDBProject/SearchViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewController.swift
+//  TMDBProject
+//
+//  Created by 이도헌 on 2022/08/03.
+//
+
+import Foundation
---
+++ TMDBProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBProject
+//
+//  Created by 이도헌 on 2022/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
